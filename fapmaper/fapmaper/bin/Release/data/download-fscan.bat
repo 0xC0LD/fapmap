02ba2c66a30b14888a2e99430539ee02fd3da3b7
0xC0LD <coldmail@gmail.com>
2020-04-19 16:31:36 +0200

    v29 - fixed fscan downloader

--- fapmaper/fapmaper/bin/Release/data/download-fscan.bat
+++ fapmaper/fapmaper/bin/Release/data/download-fscan.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Downloading file...
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/0xC0LD/fscan/raw/master/fscan/bin/Release/fscan.exe', 'fscan.exe')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/0xC0LD/fscan/raw/master/fscan/fscan/bin/Release/fscan.exe', 'fscan.exe')"
 echo Done!
